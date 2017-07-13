a3f3e81d98a1c3abdbceab879773cf612714a24a
fsilvasampaio <fsilvasampaio@gmail.com>
2017-07-13 17:26:00 -0300

    Add files via upload

---
+++ readserial.py
@@ -0,0 +1,33 @@
+'''
+This simple script will get values from serial port.
+Useful for debugging ;)
+
+References:
+
+http://www.pythonforbeginners.com/basics/getting-user-input-from-the-keyboard
+
+http://www.instructables.com/id/Interface-Python-and-Arduino-with-pySerial/
+
+'''
+
+from time import sleep
+import serial
+
+# "raw_input" is used to read text (strings) from the user
+# "port" variable will get the port address
+port = raw_input('Tell me the port address:')
+
+# Establish the connection on a specific port
+ser = serial.Serial(port, 115200)
+
+counter = 32 # Below 32 everything in ASCII is gibberish
+
+while True:
+     counter +=1
+     ser.write(str(chr(counter))) # Convert the decimal number to ASCII then send it to the Arduino
+     print ser.readline() # Read the newest output from the Arduino
+     sleep(.1) # Delay for one tenth of a second
+     if counter == 255:
+
+     	counter = 32
+
